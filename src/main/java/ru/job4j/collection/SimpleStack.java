c08ab0460dcca6d79c493ef0a8f85fd71e90449f
Mars <knownasmars@gmail.com>
2022-09-30 22:57:50 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -1,13 +1,13 @@
 package ru.job4j.collection;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+public class SimpleStack<T> {
 
-public class SimpleStack<T> implements Iterable<T> {
-
-    private Node<T> head;
+//    private Node<T> head;
 
     private ForwardLinked<T> linked = new ForwardLinked<T>();
+
+    public SimpleStack() {
+    }
 
     public T pop() {
         return linked.deleteFirst();
@@ -17,26 +17,8 @@ public class SimpleStack<T> implements Iterable<T> {
         linked.addFirst(value);
     }
 
-    @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-            Node<T> node = head;
-
-            @Override
-            public boolean hasNext() {
-                return node != null;
-            }
-
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                T value = node.value;
-                node = node.next;
-                return value;
-            }
-        };
+    public boolean isEmpty() {
+        return !linked.iterator().hasNext();
     }
 
     private static class Node<T> {
