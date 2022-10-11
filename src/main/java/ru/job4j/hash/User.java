073b3381f73c569cb1709ec71187e0db747bf972
Mars <knownasmars@gmail.com>
2022-10-11 19:17:14 +0300

    2. Без переопределения equals и hashCode [#1005 #350849]

--- src/main/java/ru/job4j/hash/User.java
+++ src/main/java/ru/job4j/hash/User.java
@@ -1,6 +1,8 @@
 package ru.job4j.hash;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
     private String name;
@@ -11,5 +13,24 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        User u1 = new User("Артем", 0, Calendar.getInstance());
+        int hashcode1 = u1.hashCode();
+        int hash1 = hashcode1 ^ (hashcode1 >>> 16);
+        int bucket1 = hash1 & 15;
+        User u2 = new User("Артем", 0, Calendar.getInstance());
+        int hashcode2 = u2.hashCode();
+        int hash2 = hashcode2 ^ (hashcode2 >>> 16);
+        int bucket2 = hash2 & 15;
+        map.put(u1, new Object());
+        map.put(u2, new Object());
+        System.out.printf("u1 - hashcode: %s, hash: %s, bucket: %s " + System.lineSeparator(),
+                hashcode1, hash1, bucket1);
+        System.out.printf("u2 - hashcode: %s, hash: %s, bucket: %s" + System.lineSeparator(),
+                hashcode2, hash2, bucket2);
+        System.out.println(u1.equals(u2));
     }
 }
