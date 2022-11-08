b1ede22858270acf50465164b602d116d989741f
Mars <knownasmars@gmail.com>
2022-11-09 00:04:11 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,34 +1,21 @@
 package ru.job4j.question;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Analize {
     public static Info diff(Set<User> previous, Set<User> current) {
         Info info = new Info(0, 0, 0);
-        Map<Integer, String> map = new HashMap<>();
+        Map<Integer, String> map =
+                current.stream()
+                        .collect(Collectors.toMap(User::getId, User::getName));
         for (User u : previous) {
-            map.put(u.getId(), u.getName());
-        }
-        for (User u : current) {
             if (!map.containsKey(u.getId())) {
-                info.setAdded(info.getAdded() + 1);
+                info.setDeleted(info.getDeleted() + 1);
             }
             if (map.containsKey(u.getId())
                     && !Objects.equals(map.get(u.getId()), u.getName())) {
                 info.setChanged(info.getChanged() + 1);
-            }
-            if (previous.contains(u) && !current.contains(u)) {
-                info.setDeleted(info.getDeleted() + 1);
-            }
-        }
-        if (previous.size() > current.size()) {
-            for (User u : previous) {
-                if (!current.contains(u)) {
-                    info.setDeleted(info.getDeleted() + 1);
-                }
             }
         }
         return info;
