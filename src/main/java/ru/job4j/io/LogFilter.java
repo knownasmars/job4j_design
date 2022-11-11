dac8756498ba658be85b096c45fe59b51b856bf5
Mars <knownasmars@gmail.com>
2022-11-11 16:32:17 +0300

    0.3. BufferedReader. [#252489 #359763]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -25,6 +25,8 @@ public class LogFilter {
     public static void main(String[] args) {
         LogFilter logFilter = new LogFilter();
         List<String> log = logFilter.filter("log.txt");
-        System.out.println(log);
+        for (String s : log) {
+            System.out.println(s);
+        }
     }
 }
