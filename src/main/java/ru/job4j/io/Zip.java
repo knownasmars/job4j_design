ab8269895979b564c0a57c1b62f7adcc4b2dbf51
Mars <knownasmars@gmail.com>
2022-11-24 11:21:52 +0300

    5.2. Архивировать проект [#861 #363359]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -1,24 +1,29 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    private final Map<String, String> argsMap = new HashMap<>();
+
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(
                 new BufferedOutputStream(
                         new FileOutputStream(target)))) {
-            for (File source : sources) {
+            for (Path source : sources) {
                 zip.putNextEntry(
-                        new ZipEntry(source.getPath()));
+                        new ZipEntry(source.toFile().getPath()));
                 try (BufferedInputStream out = new BufferedInputStream(
-                        new FileInputStream(source))) {
+                        new FileInputStream(source.toFile()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -41,25 +46,49 @@ public class Zip {
         }
     }
 
+    private static void validate(ArgsName argsName) {
+        File file = new File(argsName.get("d"));
+        if (!Files.isDirectory(file.toPath())) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", file.toPath()));
+        }
+        if (!argsName.get("e").startsWith(".")
+                && argsName.get("e").length() <= 1) {
+            throw new IllegalArgumentException(
+                    "The argument has to be started with \".\""
+            );
+        }
+        if (argsName.get("o").startsWith(".")
+                && argsName.get("o").endsWith(".zip")) {
+            throw new IllegalArgumentException(
+                    "Check out the name or format of file (has to be .zip)"
+            );
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        Search.validate(args);
-        String d = "-d=c:\\projects\\job4j_design\\";
-        args[0] = d;
-        String e = "-e=.class";
-        args[1] = e;
-        String o = "-o=project.zip";
-        args[2] = o;
-        ArgsName key = ArgsName.of(args);
-        Predicate<Path> condition = x -> !x.endsWith(key.get("e"));
-        Path root = Paths.get(key.get("d"));
-        List<File> sources = Search.search(root, condition)
-                .stream()
-                .map(Path::toFile)
-                .toList();
+        if (args.length != 3) {
+            throw new IllegalArgumentException(
+                    "Number of array elements is not correct"
+            );
+        }
+        if (!args[0].startsWith("-")) {
+            throw new IllegalArgumentException(
+                    "Arguments have to be started with dash \"-\" sign"
+            );
+        }
         Zip zip = new Zip();
+        ArgsName argsName = ArgsName.of(args);
+        validate(argsName);
+        Predicate<Path> condition =
+                x -> !x.toFile()
+                        .getName()
+                        .endsWith(argsName.get("e"));
+        Path root = Paths.get(argsName.get("d"));
+        List<Path> sources = Search.search(root, condition);
         zip.packFiles(
                 sources,
-                new File(key.get("o"))
+                new File(argsName.get("o"))
         );
         zip.packSingleFile(
                 new File("./pom.xml"),
