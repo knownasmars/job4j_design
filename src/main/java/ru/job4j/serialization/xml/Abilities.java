e1484d9894afc8a67c6147bd2a994de8f40a6610
Mars <knownasmars@gmail.com>
2022-12-06 19:41:28 +0300

    4. JAXB. Преобразование XML в POJO. [#315063]

--- src/main/java/ru/job4j/serialization/xml/Abilities.java
+++ src/main/java/ru/job4j/serialization/xml/Abilities.java
@@ -1,7 +1,18 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement(name = "ability")
 public class Abilities {
-    private final String ability;
+
+    @XmlAttribute
+    private String ability;
+
+    public Abilities() {
+
+    }
 
     public Abilities(String ability) {
         this.ability = ability;
--- src/main/java/ru/job4j/serialization/xml/Hero.java
+++ src/main/java/ru/job4j/serialization/xml/Hero.java
@@ -1,11 +1,30 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.annotation.*;
+
+@XmlRootElement(name = "hero")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Hero {
-    private final boolean protogonism;
-    private final int rating;
-    private final String name;
-    private final Abilities abilities;
-    private final String[] enemies;
+
+    @XmlAttribute
+    private boolean protogonism;
+
+    @XmlAttribute
+    private int rating;
+
+    @XmlAttribute
+    private String name;
+
+    @XmlElement
+    private Abilities abilities;
+
+    @XmlElementWrapper(name = "enemies")
+    @XmlElement(name = "enemy")
+    private String[] enemies;
+
+    public Hero() {
+
+    }
 
     public Hero(boolean protogonism, int rating, String name,
                 Abilities abilities, String[] enemies) {
