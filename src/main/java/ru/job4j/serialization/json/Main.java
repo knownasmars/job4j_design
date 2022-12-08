71e35a55e8e5dbe61f082a3a192ce28ebb0f3cf4
Mars <knownasmars@gmail.com>
2022-12-08 10:54:21 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064 #368691]

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -1,21 +1,17 @@
 package ru.job4j.serialization.json;
 
-import com.google.gson.internal.bind.JsonAdapterAnnotationTypeAdapterFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         JSONObject jsonCar = new JSONObject("{\"brand\":\"Ferrari\"}");
-
         List<String> list = new ArrayList<>();
         list.add("person1");
         list.add("person2");
         JSONArray jsonInhabitants = new JSONArray(list);
-
         final House house = new House(true, 2, "ZK Javist",
                 new Car("Lambo"), new String[]{"Alex", "Marry"});
         JSONObject jsonObject = new JSONObject();
