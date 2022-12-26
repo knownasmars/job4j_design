42c4fddae50a98b4e26c8b0ff789606e0c3a698a
Mars <knownasmars@gmail.com>
2022-12-26 14:23:35 +0300

    2. Поиск файлов по критерию [#783]

--- src/main/java/ru/job4j/hometask/io/FileSeeker.java
+++ src/main/java/ru/job4j/hometask/io/FileSeeker.java
@@ -3,13 +3,11 @@ package ru.job4j.hometask.io;
 import ru.job4j.io.ArgsName;
 import ru.job4j.io.Search;
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * 1. Программа для поиска файлов.
@@ -18,7 +16,6 @@ import java.util.regex.Pattern;
  * 4. Программа должна запускаться с параметрами, например:  -d=c:/ -n=*.?xt -t=mask -o=log.txt
  * 5. Программа должна записывать результат в файл.
  * 6. В программе должна быть валидация ключей и подсказка.
- * 7. ".*xt"
  */
 
 public class FileSeeker {
@@ -36,11 +33,11 @@ public class FileSeeker {
             String mask = values.get("n");
             String tmp = "";
             System.out.println(mask);
-            if (mask.contains(".")) {
-                tmp = mask.replace(".", "*");
-            }
             if (mask.contains("*")) {
-                tmp = mask.replace("*", "\\B");
+                tmp = mask.replace("*", ".");
+            }
+            if (tmp.contains("?")) {
+                tmp = tmp.replace("?", "*");
             }
             final String newRegex = tmp;
             System.out.println(newRegex);
@@ -63,16 +60,6 @@ public class FileSeeker {
                 .toList();
     }
 
-//    public static void match(String regex, List<String> listFiles) {
-//        Pattern pattern = Pattern.compile(regex);
-//        for (String listFile : listFiles) {
-//            Matcher matcher = pattern.matcher(listFile);
-//            while(matcher.find()) {
-//                System.out.println(matcher.group());
-//            }
-//        }
-//    }
-
     public static void write(ArgsName values, List<String> listFiles) {
         try (BufferedWriter br = new BufferedWriter(
                 new FileWriter(values.get("o")))) {
@@ -87,26 +74,36 @@ public class FileSeeker {
     }
 
     public static void validate(String[] args) {
-//        if (args[0].length() == 0) {
-//            throw new IllegalArgumentException(
-//                    "First argument cannot be empty");
-//        }
-//        File file = new File(args[0]);
-//        if (!Files.exists(file.toPath())) {
-//            throw new IllegalArgumentException(
-//                    String.format("Not exist %s", file.toPath()));
-//        }
-//        if (!Files.isDirectory(file.toPath())) {
-//            throw new IllegalArgumentException(
-//                    String.format("Not directory %s", file.toPath()));
-//        }
-//        if (args[1].length() <= 1) {
-//            throw new IllegalArgumentException(
-//                    "Second argument length has to be more than 1");
-//        }
-//        if (!args[1].startsWith(".")) {
-//            throw new IllegalArgumentException(
-//                    "Second argument has to be started with \".\"");
-//        }
+        if (args[0].length() == 0) {
+            throw new IllegalArgumentException(
+                    "First argument cannot be empty");
+        }
+        if (args.length != 4) {
+            throw new IllegalArgumentException(
+                    "Number of arguments is not valid");
+        }
+        File file = new File(args[0]);
+        if (!Files.exists(file.toPath())) {
+            throw new IllegalArgumentException(
+                    String.format("Not exist %s", file.toPath()));
+        }
+        if (!Files.isDirectory(file.toPath())) {
+            throw new IllegalArgumentException(
+                    String.format("Not directory %s", file.toPath()));
+        }
+        if (args[1].length() <= 1) {
+            throw new IllegalArgumentException(
+                    "Second argument length has to be more than 1");
+        }
+        if (!args[3].endsWith(".txt")) {
+            throw new IllegalArgumentException(
+                    "The file must have \".txt\" format");
+        }
+        if (!"mask".equals(args[2])
+                && !"name".equals(args[2])
+                && !"regex".equals(args[2])) {
+            throw new IllegalArgumentException(
+                    "Check out the input parameters. It might be \"name\", \"mask\" or \"regex\"");
+        }
     }
 }
