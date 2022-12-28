b2134ae0a41f8dcc919c96bb7f64606edcf8e4ab
Mars <knownasmars@gmail.com>
2022-12-28 22:21:08 +0300

    1. Схема прав пользователи и роли. [#1093 #375550]

--- db/1. Схема прав пользователи и роли. [#1093 #375550]/create.sql
+++ db/1. Схема прав пользователи и роли. [#1093 #375550]/create.sql
@@ -1,10 +1,9 @@
-/* 	
--- CREATE TABLES FOR RELATIONS BASE STRUCTURE -- 
-
-// users - role = many-to-one 
+	
+-- CREATE TABLES FOR RELATIONS BASE STRUCTURE 
+-- users - role = many-to-one 
 CREATE TABLE roles(
 	id serial primary key,
-	name text,
+	name text
 );
 
 CREATE TABLE users(
@@ -13,50 +12,51 @@ CREATE TABLE users(
 	role_id int references roles(id)
 );
 
-// role - rules = many-to-many
-
+-- role - rules = many-to-many
 CREATE TABLE rules(
 	id serial primary key,
-	name text;
+	name text
 );
 
 CREATE TABLE roles_rules(
 	id serial primary key,
 	role_id int references roles(id),
-	rule_id int references rules(id),
+	rule_id int references rules(id)
 );
 
-// item - users = many-to-one
+-- item - users = many-to-one
 CREATE TABLE items(
 	id serial primary key,
 	name text,
 	user_id int references users(id)
 );
 
-// item - comments = one-to-many
+-- item - comments = one-to-many
 CREATE TABLE comments(
 	id serial primary key, 
 	comment text,
 	item_id int references items(id)
 );
 
-// item - attachs = one-to-many
+-- item - attachs = one-to-many
 CREATE TABLE attachs(
 	id serial primary key,
 	attach text,
 	item_id int references items(id)
 );
 
-// item - category = many-to-one
+-- item - category = many-to-one
 CREATE TABLE categories(
 	id serial primary key,
 	category text
 );
+ALTER TABLE items
+ADD COLUMN category_id int references categories(id);
 
-// item - state = many-to-one
+-- item - state = many-to-one
 CREATE TABLE states(
 	id serial primary key,
 	state text
 );
-*/
-
+ALTER TABLE items
+ADD COLUMN state_id int references states(id);
--- db/1. Схема прав пользователи и роли. [#1093 #375550]/insert.sql
+++ db/1. Схема прав пользователи и роли. [#1093 #375550]/insert.sql
@@ -1,67 +1,58 @@
-/*
-1) 
 INSERT INTO roles(name) VALUES ('Java Developer');
 INSERT INTO roles(name) VALUES ('Java Boss');
 INSERT INTO roles(name) VALUES ('Intern');
-2)
+
 INSERT INTO users(name, role_id) VALUES ('Mars', 1);
 INSERT INTO users(name, role_id) VALUES ('Petr', 2);
 INSERT INTO users(name, role_id) VALUES ('Andrey', 1);
-3) 
+
 INSERT INTO rules(name) VALUES ('Extended');
 INSERT INTO rules(name) VALUES ('Simple');
 INSERT INTO rules(name) VALUES ('Minimal');
-4)
+
 INSERT INTO roles_rules(role_id, rule_id) VALUES (1, 2);
 INSERT INTO roles_rules(role_id, rule_id) VALUES (2, 1);
 INSERT INTO roles_rules(role_id, rule_id) VALUES (3, 3);
-5)
+
 INSERT INTO items(name, user_id) VALUES ('item_first', 1);
 INSERT INTO items(name, user_id) VALUES ('item_second', 1);
 INSERT INTO items(name, user_id) VALUES ('item_third', 2);
 INSERT INTO items(name, user_id) VALUES ('item_fourth', 2);
 INSERT INTO items(name, user_id) VALUES ('item_fifth', 3);
 INSERT INTO items(name, user_id) VALUES ('item_sixth', 3);
-6)
+
 INSERT INTO comments(comment, item_id) VALUES('Urgent', 1);
 INSERT INTO comments(comment, item_id) VALUES('Non-urgent', 2);
 INSERT INTO comments(comment, item_id) VALUES('Urgent', 3);
 INSERT INTO comments(comment, item_id) VALUES('Non-urgent', 4);
 INSERT INTO comments(comment, item_id) VALUES('Non-urgent', 5);
 INSERT INTO comments(comment, item_id) VALUES('Urgent', 6);
-7)
+
 INSERT INTO attachs(attach, item_id) VALUES('pdf', 1);
 INSERT INTO attachs(attach, item_id) VALUES('pdf', 2);
 INSERT INTO attachs(attach, item_id) VALUES('pdf', 3);
 INSERT INTO attachs(attach, item_id) VALUES('jpg', 4);
 INSERT INTO attachs(attach, item_id) VALUES('ps', 5);
 INSERT INTO attachs(attach, item_id) VALUES('jpeg', 6);
-8)
+
 INSERT INTO categories(category) VALUES('Hard');
 INSERT INTO categories(category) VALUES('Medium');
 INSERT INTO categories(category) VALUES('Easy');
-9)
-ALTER TABLE items
-ADD COLUMN category_id int references categories(id);
-10)
+
 UPDATE items SET category_id = 1 WHERE id = 1;
 UPDATE items SET category_id = 3 WHERE id = 2;
 UPDATE items SET category_id = 3 WHERE id = 3;
 UPDATE items SET category_id = 1 WHERE id = 4;
 UPDATE items SET category_id = 2 WHERE id = 5;
 UPDATE items SET category_id = 3 WHERE id = 6;
-11) 
-ALTER TABLE items
-ADD COLUMN state_id int references states(id);
-12)
+
 INSERT INTO states(state) VALUES('Active');
 INSERT INTO states(state) VALUES('Closed');
 INSERT INTO states(state) VALUES('Analyzed');
-13)
+
 UPDATE items SET state_id = 1 WHERE id = 1;
 UPDATE items SET state_id = 1 WHERE id = 2;
 UPDATE items SET state_id = 1 WHERE id = 3;
 UPDATE items SET state_id = 3 WHERE id = 4;
 UPDATE items SET state_id = 3 WHERE id = 5;
 UPDATE items SET state_id = 2 WHERE id = 6;
-*/
