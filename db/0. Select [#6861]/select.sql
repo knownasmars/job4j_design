7e00ff8c3c80637af1e5755fe9743fb8786b2590
Mars <knownasmars@gmail.com>
2022-12-29 12:31:02 +0300

    0. Select [#6861 #375838]

--- db/0. Select [#6861]/select.sql
+++ db/0. Select [#6861]/select.sql
@@ -7,16 +7,16 @@ create table fauna (
 insert into 
 	fauna(name, avg_age, discovery_date) 
 values
-	('Lion', 25, '01.02.1900'),
-	('Bat', 13, '01.03.2000'),
-	('Owl', 3000, '01.10.1400'),
+	('Lion', 25, '1900-02-01'),
+	('Bat', 13, '2000-03-01'),
+	('Owl', 3000, '1400-10-01'),
 	('Fish', 12, null),
-	('Snake', 15000, '03.03.1000');
+	('Snake', 15000, '1000-03-03');
 
 select * from fauna where name LIKE '%Fish%';
 select * from fauna where avg_age >= 10000 OR avg_age <= 21000;
 select * from fauna where discovery_date ISNULL;
-select * from fauna where discovery_date < '01.01.1950';
+select * from fauna where discovery_date < '1950-01-01';
 
 
 
